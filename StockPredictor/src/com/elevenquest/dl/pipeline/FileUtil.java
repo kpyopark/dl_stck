8a762287bfbe398dba068fc92ce8e94938ab6307
toheaven <toheavener01@gmail.com>
2017-12-25 18:13:15 +0900

    Remove depedency erros to attach new version of jackson libs.

--- StockPredictor/src/com/elevenquest/dl/pipeline/FileUtil.java
+++ StockPredictor/src/com/elevenquest/dl/pipeline/FileUtil.java
@@ -1,29 +1,60 @@
 package com.elevenquest.dl.pipeline;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.IOException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 import java.util.StringTokenizer;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.amazonaws.auth.profile.ProfileCredentialsProvider;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.ListObjectsRequest;
+import com.amazonaws.services.s3.model.ObjectListing;
+import com.amazonaws.services.s3.model.S3ObjectSummary;
 import com.elevenquest.dl.pipeline.dao.DailyStockDao;
 
 
-
+/**
+ * 
+ * /mldata-base
+ *              /model - includes model.zip file list
+ *              /BaseML - base learning data rows from 2012/01/01
+ *              /yyyyMMdd - predict target data.
+ * 
+ * @author user
+ *
+ */
 public class FileUtil {
-
-	public final static String CSV_FILE_FOLDER_PATH = "E:/Document/04.AI";
-    public final static String MODEL_DIR = "E:/Document/04.AI/Model";
+	private static Logger log = LoggerFactory.getLogger(MetricCreator.class);
+	
+	private final static AmazonS3 s3client = new AmazonS3Client(new ProfileCredentialsProvider());
+	
+	private final static String FILE_SEPARATOR = "/";
+	public final static String CSV_FILE_FOLDER_PATH_S3 = "s3://mldata-base";
+	public final static String CSV_FILE_FOLDER_PATH_FS = "E:/Document/04.AI";
+    public final static String MODEL_DIR_S3 = "S3://mldata-base/model";
+    public final static String MODEL_DIR_FS = "E:/Document/04.AI/Model";
     public final static String BASE_DATA_FOLDER = "BaseML";
-    public final static String DEFAULT_MODEL_PATH = "E:/Document/Model.zip";
+    public final static String DEFAULT_MODEL_PATH = MODEL_DIR_FS + FILE_SEPARATOR + "Model.zip";
     public final static String ML_BASE_DATE = "20120101";
+    
+    public final static boolean LOCAL_FILE_SYSTEM = "true".equals(System.getProperty("S3_ENABLED"));
+    /*
     final static String[] TRAIN_CSV_PATHS = { 
     		"E:/Document/A005930_20130101_org.csv", 
     		"E:/Document/A008560_20130101_org.csv",
     		"E:/Document/A114800_20160501_org.csv" };
-    final static String[] PREDICT_CSV_PATHS = { "E:/Document/04.AI/A042670_20160101.csv" };
-    	// { "E:/Document/A005930_20160101_org.csv", "E:/Document/A008560_20160101_org.csv" };
+    */
+    // final static String[] PREDICT_CSV_PATHS = { "E:/Document/04.AI/A042670_20160101.csv" };
+    // { "E:/Document/A005930_20160101_org.csv", "E:/Document/A008560_20160101_org.csv" };
 
     public static SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("yyyyMMdd");
     
@@ -46,7 +77,10 @@ public class FileUtil {
     }
     
     public static String getDailyPrdictTargetDirectory(String currentWorkDay, List<String> closeDays) {
-    	return CSV_FILE_FOLDER_PATH + File.separator + getNextWorkday(currentWorkDay, closeDays);
+    	if(LOCAL_FILE_SYSTEM)
+    		return CSV_FILE_FOLDER_PATH_FS + File.separator + getNextWorkday(currentWorkDay, closeDays);
+    	else
+    		return CSV_FILE_FOLDER_PATH_S3 + FILE_SEPARATOR + getNextWorkday(currentWorkDay, closeDays);
     }
     
     public static String getDailyPredictTargetFilePath(String currentworkDay, List<String> closedDays, String stockId, String startDate) {
@@ -55,7 +89,10 @@ public class FileUtil {
     }
     
     public static String getLearningBasePath() {
-    	return CSV_FILE_FOLDER_PATH + File.separator + BASE_DATA_FOLDER;
+    	if(LOCAL_FILE_SYSTEM)
+    		return CSV_FILE_FOLDER_PATH_FS + File.separator + BASE_DATA_FOLDER;
+    	else
+    		return CSV_FILE_FOLDER_PATH_S3 + FILE_SEPARATOR + BASE_DATA_FOLDER;
     }
     
 	public static String getBaseDataCsvFilePath(String startDate, String stockId) {
@@ -63,22 +100,68 @@ public class FileUtil {
 	}
 	
 	public static String getStockIdFromBaseDataCsvFilePath(String filePath) {
-		File file = new File(filePath);
-		String fileName = file.getName();
+		String fileName = filePath;
+		StringTokenizer folderFileDelimeter = new StringTokenizer(filePath, "/\\");
+		while(folderFileDelimeter.hasMoreTokens()) {
+			fileName = folderFileDelimeter.nextToken();
+		}
 		StringTokenizer st = new StringTokenizer(fileName, "_.");
 		return st.nextToken();
 	}
 
 	public static String getStartDateFromBaseDataCsvFilePath(String filePath) {
-		File file = new File(filePath);
-		String fileName = file.getName();
+		String fileName = filePath;
+		StringTokenizer folderFileDelimeter = new StringTokenizer(filePath, "/\\");
+		while(folderFileDelimeter.hasMoreTokens()) {
+			fileName = folderFileDelimeter.nextToken();
+		}
 		StringTokenizer st = new StringTokenizer(fileName, "_.");
 		st.nextToken();
 		return st.nextToken();
 	}
 
     public static String getStockModelPath(String startDate, String stockId) {
-    	return MODEL_DIR + File.separator + stockId + "_" + startDate + ".zip";
+    	if(LOCAL_FILE_SYSTEM)
+    		return MODEL_DIR_FS + File.separator + stockId + "_" + startDate + ".zip";
+    	else
+    		return MODEL_DIR_S3 + FILE_SEPARATOR + stockId + "_" + startDate + ".zip";
+    }
+    
+    public static Object[] getBucketAndKey(String path) {
+		StringTokenizer st = new StringTokenizer(path.substring("s3://".length()), "/");
+		String bucketName = st.nextToken();
+		String key = path.substring("s3://".length() + bucketName.length() + "/".length());
+		return new Object[]{ bucketName, key };
+    }
+    
+    public static List<String> getFileList(String path) throws IOException {
+    	List<String> rtn = new ArrayList<String>();
+    	if(path.indexOf("s3") == 0) {
+    		// s3 file system.
+    		ListObjectsRequest request = new ListObjectsRequest();
+    		Object[] bucketAndPrefix = getBucketAndKey(path);
+    		String prefix = (String)bucketAndPrefix[1];
+    		request.setBucketName((String)bucketAndPrefix[0]);
+    		request.setPrefix(prefix);
+    		ObjectListing objects = s3client.listObjects(request);
+    		for(S3ObjectSummary objectSummary : objects.getObjectSummaries()) {
+    			log.debug(objectSummary.toString());
+    			if(objectSummary.getSize() > 0)
+    				rtn.add(objectSummary.getKey().substring(prefix.length()));
+    		}
+    	} else {
+    		// normal file system.
+    		File mldataDir = new File(path);
+    		File[] files = mldataDir.listFiles();
+    		for(File file : files) rtn.add(file.getName());
+    	}
+    	return rtn;
+    }
+    
+    public static void main(String[] args) throws IOException {
+    	for(String name : getFileList("s3://mldata-base/model/")) {
+    		log.info("filename : [" + name + "]");
+    	}
     }
     
 }
